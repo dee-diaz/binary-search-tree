3cdbfdbffbdeb590012fd7eda47377787b994b77
dee-diaz <diazthediablo@yahoo.com>
2025-12-10 18:16:46 -0300

    implement deleteItem with all three deletion cases

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -17,7 +17,7 @@ class Tree {
     if (this.root === null) {
       this.root = newNode;
       return;
-    };
+    }
 
     let currentNode = this.root;
 
@@ -34,6 +34,44 @@ class Tree {
     currentNode.data > value
       ? (currentNode.left = newNode)
       : (currentNode.right = newNode);
+  }
+
+  deleteItem(value) {
+    const removeNode = function (node, value) {
+      if (node == null) {
+        return null;
+      }
+      if (value == node.data) {
+        // node has no children
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // node has no left child
+        if (node.left == null) {
+          return node.right;
+        }
+        // node has no right child
+        if (node.right == null) {
+          return node.left;
+        }
+        // node has two children
+        let tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, value);
+        return node;
+      } else {
+        node.right = removeNode(node.right, value);
+        return node;
+      }
+    };
+
+    this.root = removeNode(this.root, value);
   }
 }
 
