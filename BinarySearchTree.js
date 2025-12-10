8577d590bb380314abb6aea4aa30199ee229a6e9
dee-diaz <diazthediablo@yahoo.com>
2025-12-10 14:12:52 -0300

    implement insert method with duplicate prevention

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -11,6 +11,30 @@ class Tree {
     this.array = array;
     this.root = buildTree();
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    };
+
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (currentNode.data === value) return;
+
+      if (value < currentNode.data && currentNode.left !== null) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.data && currentNode.right !== null) {
+        currentNode = currentNode.right;
+      } else break;
+    }
+
+    currentNode.data > value
+      ? (currentNode.left = newNode)
+      : (currentNode.right = newNode);
+  }
 }
 
 function buildTree(array) {
